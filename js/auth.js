cb6f92a9d88f8529bd0dde55904b5cbd2d7066bd
Danilovir <danilo.vieira@fatecmatao.local>
2026-03-20 12:19:42 -0300

    Gerenciamneto de acesso

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,27 @@
 authForm.onsubmit = function (event){
+    showItem(loading)
     event.preventDefault()
     if(authForm.submitAuthForm.innerHTML == 'Acessar'){
         firebase.auth().signInWithEmailAndPassword(authForm.email.value, 
-            authForm.password.value ).then(function (user){
-                console.log('Acessou com sucesso!')
-                console.log(user)
-            }).catch(function (error) {
+            authForm.password.value ).catch(function (error) {
                 console.log('Falha no Acesso')
                 console.log(error)
             })
     } else {
         firebase.auth().createUserWithEmailAndPassword(authForm.email.value, 
-            authForm.password.value ).then(function (user){
-                console.log('Cadastrou com sucesso')
-                console.log(user)
-            }).catch(function (error) {
+            authForm.password.value ).catch(function (error) {
                 console.log('Falha no Cadastro')
                 console.log(error)
             })
     }
 }
+
+firebase.auth().onAuthStateChanged( function (user){
+    hideItem(loading)
+    if (user) {
+        console.log('Usuario AUTENTICADO')
+        console.log(user)
+    } else {
+        console.log('Usuario não Autenticado')
+    }
+})
